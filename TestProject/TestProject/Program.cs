87fd1573e65b5948eacac7b83f66d7d5bec9db77
Senchi3 <gaboh6464@gmail.com>
2019-04-04 21:20:11 -0500

    Made the game! Needs cleaning up still.

--- TestProject/TestProject/Program.cs
+++ TestProject/TestProject/Program.cs
@@ -8,18 +8,56 @@ namespace TestProject {
     class Program {
         static void Main (string[] args) {
 
+            int lives = 5;
             string secretWord;
-            string showWord = "";
+            string shownWord = "";
+
+            Console.WriteLine("Write down a sentence all in lowercase.");
             secretWord = Console.ReadLine ();
+
             Console.Clear ();
+
             for (int i = 0; i < secretWord.Length; i++) {
                 if (!(secretWord[i] == ' ')) {
-                    showWord += "*";
+                    shownWord += "*";
                 } else {
-                    showWord += " ";
+                    shownWord += " ";
                 }
             }
-            Console.WriteLine ("Your secret word is " + showWord);
+
+            Console.WriteLine("Your secret sentence is \"" + shownWord + "\" and you have " + lives + " lives left.");
+
+            while (lives > 0 && !(shownWord == secretWord)) {
+                string charTry;
+                charTry = Console.ReadLine ();
+
+                while (string.IsNullOrWhiteSpace(charTry) || charTry.Length > 1) {
+                    Console.Clear();
+                    Console.WriteLine("Your secret sentence is \"" + shownWord + "\" and you have " + lives + " lives left.");
+                    Console.WriteLine("Please input just one letter per try.");
+                    charTry = Console.ReadLine();
+                }
+
+                if (secretWord.Contains(charTry)) {
+                    string tempWord = string.Empty;
+                    for (int i = 0; i < secretWord.Length; i++) {
+                        if (secretWord[i] == charTry[0]) {
+                            tempWord += secretWord[i];
+                        } else {
+                            tempWord += shownWord[i];
+                        }
+                    }
+                    shownWord = tempWord;
+                } else {
+                    lives--;
+                }
+
+                Console.Clear();
+                Console.WriteLine("Your secret sentence is \"" + shownWord + "\" and you have " + lives + " lives left.");
+            }
+
+            Console.WriteLine("Game finished. Press any key to close.");
+            Console.ReadKey();
         }
     }
 }
