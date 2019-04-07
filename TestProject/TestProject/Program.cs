75c6038d9ea65e5c2733df61defd047bf89969dd
Flary <gaboh6464@gmail.com>
2019-04-06 19:12:59 -0500

    TestProject changed to CSHangman

--- TestProject/TestProject/Program.cs
+++ TestProject/TestProject/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TestProject {
+namespace CSHangman {
     class Program {
         static void Main (string[] args) {
 
