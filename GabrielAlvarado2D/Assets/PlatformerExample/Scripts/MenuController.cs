84d6a616f97642949e943b4642bb1b1c76c4fae0
Senchi3 <gaboh6464@gmail.com>
2019-05-28 19:59:59 -0500

    Started preparations for platformer.

---
+++ GabrielAlvarado2D/Assets/PlatformerExample/Scripts/MenuController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+
+    const int testLength = 20;
+    Vector2 boxSize = new Vector2(200, 100);
+    
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawLine(Vector3.up * -testLength, Vector3.up * testLength);
+        Gizmos.DrawLine(Vector3.right * -testLength, Vector3.right * testLength);
+    }
+}
---
+++ GabrielAlvarado2D/Assets/PlatformerExample/Scripts/PlatformMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour {
+
+    public float horizontalSpeed = 15;
+    
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * horizontalSpeed * Time.deltaTime);
+    }
+}
