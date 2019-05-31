16ec0a13e3562b70550d712f3ba682bf8014e1d5
Senchi3 <gaboh6464@gmail.com>
2019-05-30 22:14:39 -0500

    CLASE 30/5 OWO

--- GabrielAlvarado2D/Assets/PlatformerExample/Scripts/PlatformMovement.cs
+++ GabrielAlvarado2D/Assets/PlatformerExample/Scripts/PlatformMovement.cs
@@ -6,18 +6,56 @@ public class PlatformMovement : MonoBehaviour {
 
     public float gravity = 9;
     public float horizontalSpeed = 15;
+    public Collider2D playerCollider;
+    public float jumpForce = 5;
+
+    float width { get { return playerCollider.bounds.size.x; } }
+    float height { get { return playerCollider.bounds.size.y; } }
+    Vector3 leftCorner { get { return transform.position - (Vector3.right * width / 2); } }
+    Vector3 rightCorner { get { return transform.position + (Vector3.right * width / 2); } }
 
     float verticalSpeed;
-    
-    // Start is called before the first frame update
-    void Start() {
-        
+    bool grounded;
+    float detectionDistance = 0.2f;
+
+    void Update() {
+
+        RaycastHit2D leftHit = Physics2D.Raycast(leftCorner, Vector3.down, detectionDistance);
+        RaycastHit2D rightHit = Physics2D.Raycast(rightCorner, Vector3.down, detectionDistance);
+
+        if (leftHit.collider || rightHit.collider) {
+            grounded = true;
+        } else {
+            grounded = false;
+        }
+
+        Vector3 horizontalMovement = Vector3.right * Input.GetAxis("Horizontal") * horizontalSpeed * Time.deltaTime;
+        if (!grounded) {
+            verticalSpeed -= gravity * Time.deltaTime;
+        } else {
+            if (verticalSpeed < 0) {
+                verticalSpeed = 0;
+                float distance = leftHit.distance;
+                if (distance == 0) { distance = rightHit.distance; }
+                transform.Translate(Vector3.down * distance);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                verticalSpeed = jumpForce;
+            }
+        }
+        Vector3 verticalMovement = Vector3.up * verticalSpeed * Time.deltaTime;
+        transform.Translate(horizontalMovement + verticalMovement);
+
+
     }
 
-    // Update is called once per frame
-    void Update() {
-        transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * horizontalSpeed * Time.deltaTime);
-        verticalSpeed = verticalSpeed + gravity * Time.deltaTime;
-        transform.Translate(Vector3.down * verticalSpeed * Time.deltaTime);
+    void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(leftCorner, Vector3.down * detectionDistance);
+        Gizmos.DrawRay(rightCorner, Vector3.down * detectionDistance);
+
+        Gizmos.DrawSphere(leftCorner, 0.05f);
+        Gizmos.DrawSphere(rightCorner, 0.05f);
     }
 }
