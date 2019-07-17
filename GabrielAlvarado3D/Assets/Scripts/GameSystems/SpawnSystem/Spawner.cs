f297f1eefee7a1cda8793fd9090c435e24722f88
Senchi3 <gaboh6464@gmail.com>
2019-07-16 20:49:10 -0500

    Prepared for metadata and work with XML

--- GabrielAlvarado3D/Assets/Scripts/GameSystems/SpawnSystem/Spawner.cs
+++ GabrielAlvarado3D/Assets/Scripts/GameSystems/SpawnSystem/Spawner.cs
@@ -4,7 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour {
-    
+
+    public TextAsset spawnData;
     Queue<string> spawnQueue;
     int activeEntityLimit;
     public float spawnDelay;
